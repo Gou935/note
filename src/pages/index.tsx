a1d85a32a672e83eccdd94909fe57b75bfe379a4
Gou935 <103366099+Gou935@users.noreply.github.com>
2025-01-04 02:48:47 +0800

    home to intro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,10 @@ function HomepageHeader() {
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/docs/home">
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro"
+          >
             前往筆記頁面
           </Link>
         </div>
