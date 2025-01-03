615801bd982acf49b62e1d0198f1feda295680e3
Gou935 <103366099+Gou935@users.noreply.github.com>
2025-01-04 01:09:49 +0800

    change file

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://gou935.github.io",
+  url: "https://github.com/Gou935",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/note/",
